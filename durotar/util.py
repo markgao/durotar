dba3aa34e25c049ebc17d670d8adbacca49c93ae
gx <gx@stoneopus.com>
2015-06-26 16:46:20 +0800

    add context processor

--- durotar/util.py
+++ durotar/util.py
@@ -13,3 +13,23 @@ applications, but they are documented here in case they are.
 from __future__ import absolute_import, division, print_function, with_statement
 
 from importlib import import_module
+
+
+def load_class(path):
+    """Load class from path.
+    """
+
+    mod_name, cls_name = None, None
+
+    try:
+        mod_name, cls_name = path.rsplit(".", 1)
+        mod = import_module(mod_name)
+    except AttributeError as e:
+        raise RuntimeError("Error importing %s: '%s'" % (mod_name, e))
+
+    try:
+        cls = getattr(mod, cls_name)
+    except AttributeError:
+        raise RuntimeError('%s not defined in %s' % (cls_name, mod_name))
+
+    return cls
